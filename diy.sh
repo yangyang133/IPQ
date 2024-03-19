33d4f92b381340b641add68d0ee48728ed5fc2c4
zongheng23333 <1954640276@qq.com>
2024-03-19 13:59:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g'  feeds/packages/utils/xfsprogs/Makefile
+
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
