c0743db1854c6a55b2c9bf870d40c240dbd8c937
zongheng23333 <1954640276@qq.com>
2024-05-31 10:22:56 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -19,8 +19,13 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 更改 Argon 主题背景
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-./scripts/feeds update -a  
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+./scripts/feeds update -a 
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
+
+
+
 ./scripts/feeds install -a  
+
 
